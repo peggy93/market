f51449ef072d0e32b891f2aac8fd651ab6d6ad3f
peggy93 <106169482+peggy93@users.noreply.github.com>
2023-08-23 03:02:30 +0800

    mian

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
-import TestimonialSliderTwo from "../components/common/sliders/testimonial/testimonial-slider-two";
 import TextSliderOne from "../components/common/sliders/text/text-slider-one";
 import VideoSectionThree from "../components/common/video/video-section-three";
-import FeatureSection from "../components/home-two/feature-section";
 import FeatureSectionTwo from "../components/home-two/feature-section-two";
 import HeroThumbSection from "../components/home-two/hero-thumb-section";
 import HomeTwoHeroSection from "../components/home-two/home-two-hero-section";
@@ -17,13 +15,11 @@ export default function IndexTwo() {
 			</Head>
 			<HomeTwoHeroSection />
 			<HeroThumbSection />
-			<FeatureSection />
 			<TextSliderOne />
 			<VideoSectionThree />
 			<TradingSection />
 			<FeatureSectionTwo />
 			<TradingSectionTwo />
-			<TestimonialSliderTwo />
 		</>
 	);
 }
